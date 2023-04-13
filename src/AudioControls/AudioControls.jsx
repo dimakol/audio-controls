7d5171cf71e95c0d8b343c317055c6bc8cfbbb07
Dima Kolyas <dimakol@gmail.com>
2023-04-13 18:23:07 +0300

    feat(AudioControls): adding a new control - Create an input field for adding a new control. - On submit, a new control should be added to the end of the controls list, with the filled name and the value of zero. - The input field should accept only english letters

--- src/AudioControls/AudioControls.jsx
+++ src/AudioControls/AudioControls.jsx
@@ -11,25 +11,85 @@ const controls = {
 
 const AudioControls = () => {
   const [controlsList, setControlsList] = useState([]);
+  const [controlNameInput, setControlNameInput] = useState("");
 
   useEffect(() => {
     const volume = (
-      <AudioControl key={controls.volume} label={controls.volume} value={0} />
+      <AudioControl
+        key={`${controls.volume}_1`}
+        label={controls.volume}
+        value={0}
+      />
     );
     const bass = (
-      <AudioControl key={controls.bass} label={controls.bass} value={0} />
+      <AudioControl
+        key={`${controls.bass}_2`}
+        label={controls.bass}
+        value={0}
+      />
     );
     const mid = (
-      <AudioControl key={controls.mid} label={controls.mid} value={0} />
+      <AudioControl key={`${controls.mid}_3`} label={controls.mid} value={0} />
     );
     const treble = (
-      <AudioControl key={controls.treble} label={controls.treble} value={0} />
+      <AudioControl
+        key={`${controls.treble}_4`}
+        label={controls.treble}
+        value={0}
+      />
     );
     const controlsList = [volume, bass, mid, treble];
     setControlsList(controlsList);
   }, []);
 
-  return <div className="audio-controls">{controlsList}</div>;
+  /**
+   * on input change of the control name
+   * @param {*} event
+   */
+  const handleInputChange = (event) => {
+    // remove all non-letters
+    const result = event.target.value.replace(/[^a-z]/gi, "");
+    setControlNameInput(result);
+  };
+
+  /**
+   * On submit, a new control should be added to the end of the controls list,
+   * with the filled name and the value of zero.
+   */
+  const onSubmit = () => {
+    const newControl = (
+      <AudioControl
+        key={`${controlNameInput}_${controlsList.length + 1}`}
+        label={controlNameInput}
+        value={0}
+      />
+    );
+    // create a copy of controlsList on a new reference
+    let controlsListCopy = [...controlsList];
+    // update the content of the newly created array
+    controlsListCopy.push(newControl);
+    // new reference is given to setControlsList
+    setControlsList(controlsListCopy);
+    setControlNameInput("");
+  };
+
+  return (
+    <div className="audio-controls">
+      {controlsList}
+      <div className="new-control-container">
+        <input
+          type="text"
+          className="control-name-input"
+          placeholder="Enter Control Name..."
+          value={controlNameInput}
+          onChange={handleInputChange}
+        />
+        <button className="add-button" onClick={onSubmit}>
+          Add
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default AudioControls;
